2e4eaa27d53feddad24eda2b4bf0eb239521c94c
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-01-31 12:37:35 +0700

    Add Counter Component

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card p-5">
+    <h5>{{ symbol }} {{ value || 0 }}</h5>
+    <hr>
+    <h3 class="pb-3">{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+    props: ["value", "title", "symbol"]
+}
+</script>
