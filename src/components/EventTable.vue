f0198d43f42938dff192959f18ce2886a9677cc2
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-01 14:41:27 +0700

    Update Event Table

--- src/components/EventTable.vue
+++ src/components/EventTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table table-sm table-hover table-responsive text-left table-bordered">
+  <table class="table table-hover table-responsive text-left table-bordered">
     <thead>
         <tr>
             <th scope="col">#</th>
@@ -11,20 +11,67 @@
         </tr>
     </thead>
     <tbody>
-        <!-- <tr v-for="(event, i) in events" :key="i">
+        <tr v-for="(event, i) in events" :key="i">
             <th scope="col">{{ i+1 }}</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr> -->
+            <td>{{ textPrefix(event.title, 40, '...') }}</td>
+            <td>{{ textPrefix(event.description, 40, '...') }}</td>
+            <td>{{ event.ticket_price }}</td>
+            <td>{{ new Date(event.date).toLocaleDateString('id-ID') }}</td>
+            <td>
+              <button @click="viewEvent(event.id)" type="button" class="btn btn-sm btn-primary">View</button>
+              <button @click="updateEvent(event.id)" type="button" class="btn btn-sm btn-info" v-if="isAdmin">Update</button>
+              <button @click="deleteEvent(event.id)" type="button" class="btn btn-sm btn-danger" v-if="isAdmin">Delete</button>
+            </td>
+        </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 export default {
+  name: 'EventTable',
+  props: ["events", "isAdmin"],
+  setup() {
+    const router = useRouter()
+    const store = useStore()
 
+    const textPrefix = (text, length, suffix = null) => {
+      if(text.length > length) {
+        return text.substring(0, length) + suffix
+      } else {
+        return text
+      }
+    }
+
+    const viewEvent = (id) => {
+      return router.push({
+        path: "/admin/add",
+        query: {
+          event: id
+        }
+      })
+    }
+
+    const updateEvent = (id) => {
+      return router.push({
+        path: "/admin/add",
+        query: {
+          event: id
+        }
+      })
+    }
+
+    const deleteEvent = async (id) => {
+      try {
+        await store.dispatch("deleteEvent", id)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    return { textPrefix, viewEvent, updateEvent, deleteEvent }
+  }
 }
 </script>
